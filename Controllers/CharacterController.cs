fd1079b3e15125a31a1f4d6810654c6c4a4140f0
Dorota <dorotamalgorzata.szota@gmail.com>
2023-08-10 22:42:40 +0200

    lesson 15

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -25,7 +25,12 @@ namespace dotnet_rpg.Controllers
         public ActionResult<Character> GetSingle(int id) {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }    
-
+    
+    [HttpPost]
+    public ActionResult<List<Character>> AddCharacter (Character newCharacter) {
+        characters.Add(newCharacter);
+        return Ok(characters);
+    }
 
 
 
